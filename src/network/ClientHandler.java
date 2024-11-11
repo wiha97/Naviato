5966e406357a490649c31a950014001819a8d191
JesperJigs <jesperjigelius@gmail.com>
2024-11-11 21:58:19 +0100

    Added Classes ClientHandler and ClientView. Added method printRandomCoordinate in GameBoard.

---
+++ src/network/ClientHandler.java
@@ -0,0 +1,70 @@
+package network;
+
+import models.GameBoard;
+import models.Square;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+
+
+    private GameBoard gameBoard;
+    private Square square;
+    private String ip;
+    private int port;
+
+    public ClientHandler(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        this.gameBoard = new GameBoard();
+        gameBoard.generateField();
+
+    }
+
+    Socket socket;
+
+    @Override
+    public void run() {
+        {
+
+            gameBoard.generateField();
+            try {
+                socket = new Socket(ip, port);
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                //Denna loop ska startas efter att skeppen placerats ut och man valt delay
+                while (true) {
+                    String randomShot = gameBoard.printRandomCoordinate();
+                    writer.println(randomShot);
+                    //"millis" ska vara värdet från slider
+                    Thread.sleep(5000);
+
+                    String incoming = reader.readLine();
+                    if (incoming != null) {
+                        System.out.println("Host: " + incoming);
+                    } else {
+                        System.out.println("no feed");
+                        break;
+                    }
+
+                }
+
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
+
+
+}
